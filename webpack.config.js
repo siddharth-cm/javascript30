dd3188f1d385b1b5702bc1ad71651841c1af30c4
siddharth-cm <siddharth@Siddharths-MacBook-Air.local>
2018-07-03 11:20:41 +0530

    initial-commit

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+}
